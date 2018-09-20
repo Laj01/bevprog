d3ad77ab24c2dfcbac7683bcd3d0b10513244e49
Laj01 <43353939+Laj01@users.noreply.github.com>
2018-09-20 22:38:45 +0200

    Update hello_world.cpp

--- .gitignore/hello_world.cpp
+++ .gitignore/hello_world.cpp
@@ -1,9 +1,9 @@
 #include "std_lib_facilities.h"
 
-int main()         //C++ programs start by executing the function main
+int main()
 {
-	cout << "Hello, World!\n";    //output “Hello, World!”
-	keep_window_open();           //wait for a character to be entered
+	cout << "Hello, World!\n";
+	keep_window_open();
 	return 0;
 }
 
