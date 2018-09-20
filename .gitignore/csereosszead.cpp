0beee3c634e2e2435bca772e9581d24a60d00eb3
Laj01 <43353939+Laj01@users.noreply.github.com>
2018-09-20 22:35:33 +0200

    Add files via upload

---
+++ .gitignore/csereosszead.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout<<"Kérem írja be az 'a' értékét (majd üssön 'enter'-t):\n";
+	int a;
+	cin>>a;
+	cout<<"Kérem írja be a 'b' értékét (majd üssön 'enter'-t):\n";
+	int b;
+	cin>>b;
+	cout<<"A megadott számok a=="<<a<<" és b=="<<b<<"\nA számokat most megcseréljük:\n";
+		a=a+b;
+		b=a-b;
+		a=a-b;
+	cout<<"Csere után a=="<<a<<" és b=="<<b<<"\n";
+}
+	
